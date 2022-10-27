e71f516e7cf68fc0413d096d77155f519fc9d767
Darckluso <darckluso@gmail.com>
2022-10-27 19:05:46 -0300

    Adicionar arquivos de projeto.

---
+++ 8 Kyu/YouCantCodeUnderPreassure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+/// <summary>
+///     Code as fast as you can! You need to double the integer and return it.
+/// </summary>
+public static class Kata
+{
+    public static int DoubleInteger(int n)
+    {
+        //Double the input!
+        return n * 2;
+    }
+}
