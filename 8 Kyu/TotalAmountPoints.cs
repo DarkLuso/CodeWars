f4bc211e3e259b94e9f4708f2ab334b65e2a2bca
Darckluso <darckluso@gmail.com>
2022-10-27 23:45:08 -0300

    Total amount of points

---
+++ 8 Kyu/TotalAmountPoints.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public static class Kata
+{
+
+    public static int TotalPoints(string[] games)
+    {
+
+        int points = 0;
+
+        foreach (string g in games)
+        {
+
+            int mS = Convert.ToInt32(g.Substring(0, 1));
+            int eS = Convert.ToInt32(g.Substring(2));
+
+            points += mS > eS ? 3 : (mS == eS) ? 1 : 0;
+
+        }
+
+        return points;
+    }
+}
