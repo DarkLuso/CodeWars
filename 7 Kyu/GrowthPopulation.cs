d88a3e6f65eaefde6a2e18c39421fcea03a351e4
Darckluso <darckluso@gmail.com>
2022-10-27 23:43:58 -0300

    Growth of a Population

---
+++ 7 Kyu/GrowthPopulation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Arge
+{
+
+    public static int NbYear(int p0, double percent, int aug, int p)
+    {
+
+        int x = 0;
+
+        while (p0 < p)
+        {
+
+            p0 += Convert.ToInt32(Math.Floor(p0 * (percent / 100) + aug));
+
+            x++;
+
+        }
+
+        return x;
+
+    }
+}
