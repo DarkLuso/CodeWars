62f9fe6a5ab2c16c626d56915eabefe7cf092568
Darckluso <darckluso@gmail.com>
2022-10-27 22:29:10 -0300

    String repeat

---
+++ 8 Kyu/StringRepeat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    public static class Program
+    {
+        public static string RepeatStr(int n, string s) => string.Concat(Enumerable.Repeat(s, n));
+        //{
+        //string result = "";
+        //for(int x = 0; x < n; x++) {
+        //result += s;
+        //}
+        //return result;
+        //}
+    }
+}
