1e8dabc8e3c8d75fd0036339faee2f9ab9d70ac6
Darckluso <darckluso@gmail.com>
2022-10-27 21:20:40 -0300

    Count of positives / sum of negatives

---
+++ 8 Kyu/CountOfPositives-SumOfNegatives.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] CountPositivesSumNegatives(int[] input)
+    {
+
+        int posCount = 0;
+        int negSum = 0;
+
+        if (input == null || input.Length == 0) return new int[0] { };
+
+        foreach (int n in input)
+        {
+
+            if (n > 0) posCount++;
+            else if (n < 0) negSum += n;
+        }
+
+        return new int[2] { posCount, negSum };
+
+    }
+
+}
