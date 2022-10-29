386c031921fc53a69e121ffc9e68f935f411ecaa
Darckluso <darckluso@gmail.com>
2022-10-29 04:44:22 -0300

    Exes and Ohs

---
+++ 7 Kyu/ExesAndOhs.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System;
+public static class Kata
+{
+    public static bool XO(string input)
+    {
+
+        int o = input.Where(z => char.ToUpperInvariant(z) == 'O').Count();
+        int x = input.Where(y => char.ToUpperInvariant(y) == 'X').Count();
+
+        return o == x ? true : false;
+    }
+}
