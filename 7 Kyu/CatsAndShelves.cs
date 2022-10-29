70ca8f1dacf0ec5d8b27e03fa4cc06d451f354c8
Darckluso <darckluso@gmail.com>
2022-10-29 08:12:27 -0300

    Cats and shelves

---
+++ 7 Kyu/CatsAndShelves.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Kata
+{
+    public static int Cats(int s, int f)
+    {
+        int j = 0;
+        int d = f - 2;
+
+        while (s < f) { _ = s == d ? s++ : s += 3; j++; }
+
+        return j;//Mew
+
+    }
+}
