8b467b1d8cc820347c0fff8f239fd1b3df18ec70
Darckluso <darckluso@gmail.com>
2022-10-30 20:55:02 -0300

    Basic Mathematical Operations

---
+++ 8 Kyu/BasicMathematicalOperations.cs
@@ -0,0 +1,14 @@
+﻿namespace Solution
+{
+    public static class Program
+    {
+        public static double basicOp(char o, double x, double y) => o switch
+        {
+            '+' => x + y,
+            '-' => x - y,
+            '*' => x * y,
+            '/' => x / y,
+            '%' => x % y
+        };
+    }
+}
