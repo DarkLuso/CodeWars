574da4e49dea94d11709fdb59e3e7f41931bc4f8
Darckluso <darckluso@gmail.com>
2022-10-30 10:09:56 -0300

    Object Oriented Piracy

---
+++ 8 Kyu/ObjectOrientedPiracy.cs
@@ -0,0 +1,13 @@
+﻿public class Ship
+{
+    public int Draft;
+    public int Crew;
+
+    public Ship(int draft, int crew)
+    {
+        Draft = draft;
+        Crew = crew;
+    }
+
+    public bool IsWorthIt() => Draft - (Crew * 1.5) > 20 ? true : false;
+}
