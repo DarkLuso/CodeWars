699c28eb116ebf6b862fdf2819c0552b7aa666ca
Darckluso <darckluso@gmail.com>
2022-10-30 17:14:01 -0300

    CSV representation of array

---
+++ 8 Kyu/CSVRepresentationOfArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string ToCsvText(int[][] array)
+    {
+        string r = "";
+
+        foreach (int[] a in array)
+        {
+            foreach (int n in a)
+            {
+                r += n + ",";
+            }
+
+            r = r.Remove(r.Length - 1);
+            r += "\n";
+
+        }
+        r = r.Remove(r.Length - 1);
+
+        return r;
+    }
+}
