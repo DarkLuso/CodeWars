1dc8df0fb85715c4e5372ae5bd2e776aef752f71
Darckluso <darckluso@gmail.com>
2022-10-30 12:51:00 -0300

    Duck Duck Goose

---
+++ 8 Kyu/DuckDuckGoose.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Kata
+{
+    public static string DuckDuckGoose(Player[] p, int g) => p[(g - 1) % p.Length].Name;
+}
+public class Player
+{
+    public string Name { get; set; }
+
+    public Player(string name)
+    {
+        this.Name = name;
+    }
+}
