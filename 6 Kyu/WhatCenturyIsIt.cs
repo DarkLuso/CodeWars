391804fa88174bb70ed89a747c2b238b39a30c16
Darckluso <darckluso@gmail.com>
2022-10-30 16:27:23 -0300

    What century is it?

---
+++ 6 Kyu/WhatCenturyIsIt.cs
@@ -0,0 +1,17 @@
+﻿public class Kata
+{
+    public static string WhatCentury(string year)
+    {
+        int y = int.Parse(year);
+
+        y = y % 100 == 0 ? y / 100 : y / 100 + 1;
+
+        return y / 10 == 1 ? y + "th" : (y % 10) switch
+        {
+            1 => y + "st",
+            2 => y + "nd",
+            3 => y + "rd",
+            _ => y + "th"
+        };
+    }
+}
