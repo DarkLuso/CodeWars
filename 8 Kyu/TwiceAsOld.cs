ce73f202042ea9f0d63ee2a22efb3a875a1c3f81
Darckluso <darckluso@gmail.com>
2022-10-30 21:12:57 -0300

    Twice as old

---
+++ 8 Kyu/TwiceAsOld.cs
@@ -0,0 +1,7 @@
+﻿namespace Solution
+{
+    public class TwiceAsOldSolution
+    {
+        public static int TwiceAsOld(int d, int s) => (d - s) * 2 > d ? (d - s) * 2 - d : d - (d - s) * 2;
+    }
+}
