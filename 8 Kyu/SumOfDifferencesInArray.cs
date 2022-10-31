fa9bc194aef2e0d8c20744eef742898c34373999
Darckluso <darckluso@gmail.com>
2022-10-30 21:29:50 -0300

    Sum of differences in array

---
+++ 8 Kyu/SumOfDifferencesInArray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int SumOfDifferences(int[] a)
+    {
+        int r = 0;
+        a = a.OrderByDescending(x => x).ToArray();
+
+        for (int y = 0; y + 1 < a.Length; y++)
+        {
+            r += a[y] - a[y + 1];
+        }
+
+        return r;
+    }
+}
