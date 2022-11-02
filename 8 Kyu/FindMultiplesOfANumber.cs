41f2bbfc1bba23149fb66a40ce23c2b4b39b0a96
Darckluso <darckluso@gmail.com>
2022-11-02 17:58:04 -0300

    Find Multiples of a Number

---
+++ 8 Kyu/FindMultiplesOfANumber.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static List<int> FindMultiples(int i, int l)
+    => Enumerable.Range(1, l).Where(x => x % i == 0).ToList();
+}
