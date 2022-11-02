40f3a43e44d9dffe5e6b1ddd381b47717d62ca85
Darckluso <darckluso@gmail.com>
2022-11-02 17:45:52 -0300

    Switch/Case - Bug Fixing #6

---
+++ 8 Kyu/SwitchCase-BugFixing#6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Kata
+{
+    public static double EvalObject(double value1, double value2, char o) => o switch
+    {
+        '+' => value1 + value2,
+        '-' => value1 - value2,
+        '/' => value1 / value2,
+        '*' => value1 * value2,
+        '%' => value1 % value2,
+        '^' => Math.Pow(value1, value2),
+        _ => 0
+    };
+}
