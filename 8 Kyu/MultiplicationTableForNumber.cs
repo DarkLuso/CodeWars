23d5fc36a3027b464180f87b6c3377492f9461d3
Darckluso <darckluso@gmail.com>
2022-11-02 15:43:15 -0300

    Multiplication table for number

---
+++ 8 Kyu/MultiplicationTableForNumber.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static string MultiTable(int n)
+      => String.Join("\n",
+        Enumerable.Range(0, 10)
+          .Select((x, i) => $"{i + 1} * {n} = {(i + 1) * n}"));
+}
